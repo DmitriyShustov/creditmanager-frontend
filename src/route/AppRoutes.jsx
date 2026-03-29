a251021c6893251a316e85456f1879c980a36ecc
shustov-dmitry <shustov.dm.s@gmail.com>
2026-03-29 19:35:11 +0300

    feat: add base route

--- src/route/AppRoutes.jsx
+++ src/route/AppRoutes.jsx
@@ -3,6 +3,12 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { MainWindow } from "../components/MainWindow/MainWindow";
 
 const router = createBrowserRouter(
+    [
+        {
+            path: "/",
+            element: <MainWindow />,
+        },
+    ],
     {
         basename: "/",
     }
